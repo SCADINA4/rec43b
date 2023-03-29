9f842da6e45445195d25dace5928da13c8d3e660
Aleksandr90210 <yudin.aleksandrkpc802020@mail.ru>
2023-03-29 13:25:14 +0300

    Добавьте файлы проекта.

---
+++ rec43b/ViewsModels/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using rec43b.ViewsModels.Base;
+
+
+
+namespace rec43b.ViewsModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        private int number;
+        public int Number
+        {
+            get { return number; }
+            set { Set(ref number,value); }
+        }
+
+        public int Count(int n)
+        {
+            return Number= n < 10 ? 1 : 1 + Count(n / 10);
+        }
+            
+
+    
+    }
+
+}
